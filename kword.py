06f3f012811e497819f48271d5a4b748fbdeec6f
el-presidente-kukki <67149799+el-presidente-kukki@users.noreply.github.com>
2022-03-27 12:20:57 +0200

    Updated kword.py and main.py
    kword.py
    - Added/Changed some Comments
    - Only lemmatize text for single keyword
    - Change top_n to a selectable Value

--- kword.py
+++ kword.py
@@ -5,12 +5,11 @@ import spacy
 # lemminflect shows as unused but needs to be imported!
 import lemminflect
 
-
 # Loading the Model outside of the Definiton instead of inside
 # makes the Keyword Extraction faster because it only has to load once instead of in each iteration
 kw_model = KeyBERT(model='all-mpnet-base-v2')
 # Load Spacy en Model (NLP)
-# Todo: Lemmatize Sometimes changes data to Datum....
+# Probably Fixed using lemminflect as addition: Lemmatize Sometimes changes data to Datum....
 lem_model = spacy.load("en_core_web_trf")
 
 
@@ -25,20 +24,21 @@ def preprocess(text: str, lemmatize: bool) -> str:
 
     # Lemmatize before so you dont get both: "(economic', 0.307), ('economics', 0.302)"
     # Todo: Check Keyword Results after Lemmatize -> especially Multiple Keyword!
+    # Todo: Maybe use Multiple_Keywords without Lemmatize
     if lemmatize:
         doc = lem_model(text)
         # token._.lemma() -> uses lemminflect (pip install lemminflect): better results (example: data will not/less be transformed to datum)
         lem_text = " ".join([token._.lemma() for token in doc])
-        text = lem_text
 
-    return text
+    return lem_text, text
 
 
-def extract_all(text: str, lemmatize: bool):
+def extract_all(text: str, lemmatize: bool, top_n: int):
     # Information: preprocess does not need to remove StopWords -> KeyBert includes this function
-    cleantxt = preprocess(text, lemmatize)
+    # Info: Currently MultipleKeywords standard without Lemmatize
+    singletext, multipletext = preprocess(text, lemmatize)
 
-    single = kw_model.extract_keywords(cleantxt, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=20)
-    multiple = kw_model.extract_keywords(cleantxt, keyphrase_ngram_range=(2, 3), stop_words='english', top_n=20)
+    single = kw_model.extract_keywords(singletext, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=top_n)
+    multiple = kw_model.extract_keywords(multipletext, keyphrase_ngram_range=(2, 3), stop_words='english', top_n=top_n)
 
     return single, multiple
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Imports
 import pandas as pd
 from tqdm import tqdm
-
+import json
 # Import own .py files
 import db
 import init
@@ -11,20 +11,25 @@ import kword
 
 
 def main():
-    # Settings
-    # To Control what you want to do
-    keywords = True
-    lemmatize = True
-    classification = False
-    # Write Results to Database, Save Local as csv or Both
-    csv = False
-    database = False
+    # Keywords-Settings ====
+    keywords = True         # Extract Keywords (True/False)
+    top_n_keywords = 20     # Set how many Top Keywords to extract from each text
+    lemmatize = True        # Lemmatize Single Keywords (True/False)
+
+    # Classification-Settings ====
+    classification = False  # Classification of Mails (True/False)
+
+    # Export-Settings ====
+    js = False             # Export as json (True/False)
+    database = False        # Export/Write to Database (True/False)
     if database:
         db.connect()
 
+
     # Other Variables
-    Single_Keywords = None
-    Multiple_Keywords = None
+    single_keywords = None
+    multiple_keywords = None
+    results = df = pd.DataFrame()
 
     # Import DataSet and preprocess
     dataset = init.import_data()
@@ -36,24 +41,45 @@ def main():
     for i in tqdm(range(len(dataset))):
         # Todo: Complete Process (Keyword Extraction, Classification, ...)
         # For Each Step we use the Base DataSet after it was Imported
-        # Data Cleansing is done separate for each step
+        # Data Cleansing is done separate for each step (Currently only single Keywords with Lemmatize)
         if keywords:
             keyword_text = dataset.loc[i, "Subject"] + '. ' + dataset.loc[i, "Body"]
-            single, multiple = kword.extract_all(keyword_text, lemmatize)
-            print(single)
-            print(multiple)
-            print('========================================================')
+            single, multiple = kword.extract_all(keyword_text, lemmatize, top_n_keywords)
+            #print(single)
+            #print(multiple)
+            #print('========================================================')
 
         if classification:
-            print('Mail Classification')
+            print('Mail Classification /tbd')
 
         # Todo: Write all to DataBase after element was processed
         # Todo: Implement all needed Functions
         if database:
+            # Todo: Doubled Check -> Check if there is a similar mail (If yes -> mark Keywords as doubled)
+
+            # Write Mail to DB
+            while True:
+                mail_id = db.insert_mail(dataset.loc[i, "Date_Received"], dataset.loc[i, "From_Name"],
+                                         dataset.loc[i, "From_Mail"],
+                                         dataset.loc[i, "Subject"], dataset.loc[i, "Body"])
+                mail_id -= 1
+                if mail_id == 0:
+                    print('Mail ID is ZERO')
+                    continue
+                else:
+                    break
+            # Write Keywords to DB
+            if keywords:
+                db.insert_singlekeyword(single_keywords, mail_id)
+                db.insert_multiplekeyword(multiple_keywords, mail_id)
+            # Write Classification to DB
+            if classification:
+                print('Write to Database')
             print('Write to Database')
 
-        # Todo: Implement .csv Export for all (Maybe as JSON Format?)
-        if csv:
+        # Todo: Implement JSON Export containing all values
+        # -> Late Project Stage if Time
+        if js:
             # Save in .json Format or somethin else
             print('Write to .csv')
 
@@ -61,44 +87,12 @@ def main():
 
     if database:
         db.disconnect()
+    if js:
+        # Todo: Export df as json and format it nicely
+        print('Export as json')
     print("Done")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-'''#TODO: Complete Rework of Main and other Parts
-def main():
-    # Import DataSet and preprocess
-    dataset = preprocess.import_data()
-    print(dataset.dtypes)
-
-    # Process for each Mail is exactly the same
-    # for i in range(len(dataset)):
-    # print(DataSet.loc[i, "Date_Received"], DataSet.loc[i, "From_Name"])
-
-    for i in tqdm(range(len(dataset))):
-        # print('Mail %s from %d' % (i + 1, len(DataSet.index)))
-        while True:
-            mail_id = db.insert_mail(dataset.loc[i, "Date_Received"], dataset.loc[i, "From_Name"],
-                                     dataset.loc[i, "From_Mail"],
-                                     dataset.loc[i, "Subject"], dataset.loc[i, "Body"])
-            mail_id -= 1
-            if mail_id == 0:
-                print('Mail ID is ZERO')
-                continue
-            else:
-                break
-
-        # print(DataSet.loc[i, "Date_Received"])
-        # Insert a Database Check if the content is doubled -> Sometimes a User resents the mail
-        # These Keywords should be flagged as doubled
-        keyword_text = dataset.loc[i, "Subject"] + '. ' + dataset.loc[i, "Body"]
-        single_keywords = preprocess.keywords_single(keyword_text)
-        multiple_keywords = preprocess.keywords_multiple(keyword_text)
-
-        db.insert_singlekeyword(single_keywords, mail_id)
-        db.insert_multiplekeyword(multiple_keywords, mail_id)
-
-    print("Done")'''
