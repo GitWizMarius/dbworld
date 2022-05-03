5d3d2c66ebd042ad510512abddcf5dcc4c5fb2b8
el-presidente-kukki <67149799+el-presidente-kukki@users.noreply.github.com>
2022-05-03 22:09:12 +0200

    Removed unused Import Statement and added SVM.py

--- Benchmarks/Classification/Algorithmen/random_forest.py
+++ Benchmarks/Classification/Algorithmen/random_forest.py
@@ -1,8 +1,6 @@
 import pickle
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
-from pprint import pprint
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
